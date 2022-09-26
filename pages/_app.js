3d523ca7519ec502f5690cdcea6232f5ae313b08
importTahsinZaman <tahsinz21366@gmail.com>
2022-09-26 13:14:33 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserContext } from "../lib/context.js";
+import { useUserData } from "../lib/hooks.js";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
