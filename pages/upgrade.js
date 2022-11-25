de4ab9bd757cf9e8beaa626c442782835050813b
importTahsinZaman <tahsinz21366@gmail.com>
2022-11-24 23:17:54 -0500

    Fixed Stripe Payment Button

--- pages/upgrade.js
+++ pages/upgrade.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { UserContext } from "../lib/context";
+import { useContext } from "react";
 import { CreateEducatorCheckoutSession } from "../lib/stripe.js";
 
 export default function Upgrade() {
+  const { user, username, premium } = useContext(UserContext);
   return (
     <>
-      <button className="btn-blue" onClick={CreateEducatorCheckoutSession}>
-        Upgrade
-      </button>
+      {(user, username, !premium) ? (
+        <button
+          onClick={() => CreateEducatorCheckoutSession(user, username, premium)}
+        >
+          Upgrade
+        </button>
+      ) : (
+        <>Already educator</>
+      )}
     </>
   );
 }
